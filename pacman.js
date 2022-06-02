2f52f02a9d8f34b36fe72865ad4682c1f1e59a65
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-06-02 09:59:17 +0900

    ..

--- pacman.js
+++ pacman.js
@@ -13489,26 +13489,26 @@
     });
 
     //  Mr.New - Receive message from the site
-    // window.addEventListener("message", (event) => {
-    //     console.log('# event => ', event);
-    //     if (event.origin !== URL_OF_SITE) {
-    //         console.log('event.origin !== URL_OF_SITE');
-    //         console.log('# event.data => ', event.data);
-    //         return;
-    //     } else {
-    //         console.log('# event.data => ', event.data);
-    //         console.log('# event.source => ', event.source);
-    //     }
-    //     // ...
-    // }, false);
+    window.addEventListener("message", (event) => {
+        console.log('# event => ', event);
+        if (event.origin !== URL_OF_SITE) {
+            console.log('event.origin !== URL_OF_SITE');
+            console.log('# event.data => ', event.data);
+            return;
+        } else {
+            console.log('# event.data => ', event.data);
+            console.log('# event.source => ', event.source);
+        }
+        // ...
+    }, false);
 
     // channel.addEventListener('message', (event) => {
     //     console.log(event.data);
     // });
 
-    window.onmessage = function (e) {
-        if (e.data == 'hello') {
-            alert('It works!');
-        }
-    };
+    // window.onmessage = function (e) {
+    //     if (e.data == 'hello') {
+    //         alert('It works!');
+    //     }
+    // };
 })();
